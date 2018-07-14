0c7f8403efcf5f00e2579d53612b483c0c893d41
david <davidw.rq@gmail.com>
2018-07-14 17:00:00 -0400

    feat(medrecords): add app web

---
+++ medrecords_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MedrecordsAppConfig(AppConfig):
+    name = 'medrecords_app'
---
+++ medrecords_app/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+
+class MedicalRecord(models.Model):
+    pass
